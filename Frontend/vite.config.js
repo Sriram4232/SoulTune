63894eb61aaa8bbb395e2d5bc8ec261f8ab881cc
Meka Sriram <srirammeka13@gmail.com>
2026-09-19 18:43:28 +0530

    Update proxy target URL in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 const proxyConfig = {
-  target: 'http://127.0.0.1:8000',
+  target: 'https://soultune-zctz.onrender.com/',
   changeOrigin: false,
   configure: (proxy) => {
     proxy.on('error', (err, req, res) => {
